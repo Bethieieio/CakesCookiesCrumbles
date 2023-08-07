da2e670c5dd26ae07d527b72baba31fb3fcfad6b
Beth Allaway <beth_allaway@yahoo.com>
2023-08-07 21:07:18 +0100

    set up logout function

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,12 +2,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import BootstrapNavbar from 'react-bootstrap/Navbar';
 import { useNavigate } from 'react-router-dom';
-import { useCurrentUser } from './context/CurrentUserContext';
+import { useCurrentUser, useSetCurrentUser } from './context/CurrentUserContext';
+import axios from 'axios';
 
 export const Navbar = () => {
     const navigate = useNavigate()
     const currentUser = useCurrentUser()
-    
+    const setCurrentUser = useSetCurrentUser()
+    const onSignOut = async () => {
+        try {
+            await axios.post('/logout/')
+            setCurrentUser(null)
+            navigate('/login/')
+        } catch (err) {
+        console.log(err);
+        }
+    }
+
     return(
     <BootstrapNavbar expand="lg" className="bg-body-tertiary">
         <Container>
@@ -22,7 +33,7 @@ export const Navbar = () => {
                 :
                 (<Nav className="justify-content-end">
                 <Nav.Link onClick={() => {navigate('/Create Recipe')}}> Create Recipe </Nav.Link>
-                <Nav.Link onClick={() => {navigate('/Sign Out')}}> Sign Out </Nav.Link>
+                <Nav.Link onClick={() => onSignOut()}> Sign Out </Nav.Link>
             </Nav>)}
 
             </BootstrapNavbar.Collapse>
