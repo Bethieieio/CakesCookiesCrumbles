f9329470e66a1d52aacb379bffb1eb701acb0d3a
Beth Allaway <beth_allaway@yahoo.com>
2023-08-14 21:14:27 +0100

    add recipe info and start it's page layout

--- src/components/pages/singleRecipe.js
+++ src/components/pages/singleRecipe.js
@@ -3,15 +3,21 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './singleRecipe.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/fontawesome-free-solid';
+import { faHeart as faHeartO } from '@fortawesome/free-regular-svg-icons'
 
 export const SingleRecipe = () => {
     const params = useParams()
+    const [recipe ,setRecipe] = useState()
     console.log(params);
     const getRecipe = async() => {
         const recipe = await axios.get(`/recipes/${params.id}`)
         console.log(recipe)
+        setRecipe(recipe.data)
     }
     useEffect(() => {
         getRecipe()
@@ -19,11 +25,34 @@ export const SingleRecipe = () => {
     return (
         <Container fluid>
             <Row>
-                <Col md={{ span: 6, offset: 3 }}>
+                <Col md={{ span: 10, offset: 1 }}>
+                    
                     <Card>
-                    <Card.Header as="h5">recipe title</Card.Header>
+                    <Card.Header>{recipe ?  <>
+                        <h3>{recipe.title}</h3>
+                        <FontAwesomeIcon icon={faHeartO} />
+                    </> : 'Not Found'}</Card.Header>
                     <Card.Body>
-                        put recipe here
+                        {recipe ? (<>
+                        <Container>
+                            <Row>
+                                <Col><img className='recipe-image' src={recipe.image}/></Col>
+                                <Col>
+                                    <h5>Ingredients</h5>
+                                    {recipe.ingredients.split('\n').map((line)=> <p key={line}>{line}</p>)}
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <p>{recipe.description}</p>
+                                    <h5>Instructions</h5>
+
+                                    {recipe.instructions.split('\n').map((line)=> <p key={line}>{line}</p>)}
+                                </Col>
+                            </Row>
+                        </Container>
+                        </>): (<>
+                        loading or not found </>)}
                     </Card.Body>
                     </Card>
                 </Col>
