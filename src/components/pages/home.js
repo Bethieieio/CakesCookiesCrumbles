8c79ef6945475e67b6eb9017b59ca68b766a2918
Beth Allaway <beth_allaway@yahoo.com>
2023-08-18 21:51:24 +0100

    display single recipes in pagination

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,22 +1,38 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Recipe } from "../recipe"
+import { Container, Row } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSpinner } from "@fortawesome/fontawesome-free-solid"
 
 export const Home = () => {
     const [page, setPage] = useState(1)
     const [recipes, setRecipes] = useState([])
+    const [loading, setLoading] = useState(true)
+
     const getPage = async() => {
         try {   
+            setLoading(true)
             const result = await axios.get(`recipes?page=${page}`)
             console.log(result)
+            setRecipes(result.data.results)
         }catch(error){
 
         }
+        setLoading(false)
     }
     useEffect(() => {
         getPage()
     }, [])
 
     return (
-        <h1>My new home page</h1>
+        <>
+        <Container>
+            <Row>
+                {recipes.map(recipe => <Recipe recipe={recipe}/>)}
+                {loading && <FontAwesomeIcon icon={faSpinner} spinPulse size='5x' />}
+            </Row>
+        </Container>
+        </>
     )
 }
---
+++ src/components/recipe.js
@@ -0,0 +1,39 @@
+import { faHeart } from "@fortawesome/fontawesome-free-solid"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Card, Col, Container, Image, Row } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+
+export const Recipe = (props) => {
+    const {recipe} = props
+    const navigate = useNavigate()
+    return(
+        <Col md={4}>
+            <Card>
+                <Container>
+                    <Row>
+                        <Col xs={9}>
+                            <Image onClick={() => {
+                                navigate(`/recipe/${recipe.id}`)
+                            }}fluid src={recipe.image}/>
+                        </Col>
+                        <Col xs={3}>
+                            <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col onClick={() => {
+                                navigate(`/recipe/${recipe.id}`)
+                            }}>
+                            <h4>{recipe.title}</h4>
+                            <p>{recipe.description}</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </Card>
+
+        </Col>
+    )
+}
+
