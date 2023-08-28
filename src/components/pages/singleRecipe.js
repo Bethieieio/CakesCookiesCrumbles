b3fd40d1646970287670995ce7d97f162d4d56c2
Beth Allaway <beth_allaway@yahoo.com>
2023-08-28 11:08:19 +0100

    added fonts to recipe page

--- src/components/pages/singleRecipe.js
+++ src/components/pages/singleRecipe.js
@@ -40,12 +40,12 @@ export const SingleRecipe = () => {
                         
                         {recipe ?   
                         <Container>
-                            <Row className='px-5'>
+                            <Row className='justify-content-center' xs="auto">
                                 <Col className="d-flex justify-content-center"> <FontAwesomeIcon className="single-recipe-star" aria-label='Rate Recipe' icon={faStarO} onClick={() => {
                                     if (currentUser) setRatingModalOpen(true)
                                     else navigate('/login/')
                                 }}/></Col>
-                                <Col md={{ span: 3 }} className="d-flex justify-content-center headings"> <h3>{recipe.title}</h3> </Col>
+                                <Col md={{ span: 3 }} className="d-flex justify-content-center headings"> <h3 className='h4'>{recipe.title}</h3> </Col>
                                 <Col className="d-flex justify-content-center"><FavouriteToggle id={recipe.id} favouriteId={recipe.favourites[0]?.id} /></Col>
                             </Row>
                             <Row>
@@ -63,16 +63,16 @@ export const SingleRecipe = () => {
                             <Row>
                                 <Col><img className='recipe-image' src={recipe.image} alt={`image of ${recipe.title}`}/></Col>
                                 <Col>
-                                    <h5>Ingredients</h5>
-                                    {recipe.ingredients.split('\n').map((line)=> <p key={line}>{line}</p>)}
+                                    <h5 className='asap'>Ingredients</h5>
+                                    {recipe.ingredients.split('\n').map((line)=> <p className='josefin' key={line}>{line}</p>)}
                                 </Col>
                             </Row>
                             <Row>
                                 <Col>
-                                    <p>{recipe.description}</p>
-                                    <h5>Instructions</h5>
+                                    <p className='josefin'>{recipe.description}</p>
+                                    <h5 className='asap'>Instructions</h5>
 
-                                    {recipe.instructions.split('\n').map((line)=> <p key={line}>{line}</p>)}
+                                    {recipe.instructions.split('\n').map((line)=> <p className='josefin' key={line}>{line}</p>)}
                                 </Col>
                             </Row>
                             <RatingModal title={recipe.title} open={ratingModalOpen} onClose={setRatingModalOpen} id={recipe.id}/>
